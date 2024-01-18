1cfea195172a37ce7604a2e5cfa811cc2b05890a
Konstantinos Tsolakidis <ktsolakidis@Konstantinoss-MacBook-Pro.local>
2024-01-18 12:25:36 +0100

    First

---
+++ data/news.js
@@ -0,0 +1,44 @@
+export const news = [
+  {
+    date: '27-10-2023',
+    image: 'conference.jpeg',
+    title: "Random Conference for something very important",
+    text: "This Wednesday we are having a nice snack & beers session after work @thelab, everybody is welcome for some beer and games, nice conversations and warm wishes about hollidays! The rest of the text is going to be nonsense because I just have to write something in order to test this layout. If you  are still reading you must be very bored. Stop procrastinating now. You have work to do. Still here? I'm gonna tell Nikos.",
+    link: "https://www.youtube.com/watch?v=JxS5E-kZc2s"
+  },
+  {
+    date: '27-10-2023',
+    image: 'con2.jpeg',
+    title: "Random Conference for something very important",
+    text: "This Wednesday we are having a nice snack & beers session after work @thelab, everybody is welcome for some beer and games, nice conversations and warm wishes about hollidays! The rest of the text is going to be nonsense because I just have to write something in order to test this layout. If you  are still reading you must be very bored. Stop procrastinating now. You have work to do. Still here? I'm gonna tell Nikos.",
+    link: "https://www.youtube.com/watch?v=JxS5E-kZc2s"
+  },
+  {
+    date: '27-10-2023',
+    title: "Same thing but without an image.",
+    text: "This Wednesday we are having a nice snack & beers session after work @thelab, everybody is welcome for some beer and games, nice conversations and warm wishes about hollidays! The rest of the text is going to be nonsense because I just have to write something in order to test this layout. If you  are still reading you must be very bored. Stop procrastinating now. You have work to do. Still here? I'm gonna tell Nikos.",
+    link: "https://www.youtube.com/watch?v=JxS5E-kZc2s"
+  },
+  {
+    date: '27-10-2023',
+    image: 'cat.avif',
+    title: "Random Conference for something very important",
+    text: "This Wednesday we are having a nice snack & beers session after work @thelab, everybody is welcome for some beer and games, nice conversations and warm wishes about hollidays! The rest of the text is going to be nonsense because I just have to write something in order to test this layout. If you  are still reading you must be very bored. Stop procrastinating now. You have work to do. Still here? I'm gonna tell Nikos.",
+    link: "https://www.youtube.com/watch?v=JxS5E-kZc2s"
+  },
+  {
+    date: '27-10-2023',
+    title: "Same thing but without an image.",
+    text: "This Wednesday we are having a nice snack & beers session after work @thelab, everybody is welcome for some beer and games, nice conversations and warm wishes about hollidays! The rest of the text is going to be nonsense because I just have to write something in order to test this layout. If you  are still reading you must be very bored. Stop procrastinating now. You have work to do. Still here? I'm gonna tell Nikos.",
+    link: "https://www.youtube.com/watch?v=JxS5E-kZc2s"
+  },
+  {
+    date: '27-10-2023',
+    image: 'con2.jpeg',
+    title: "Random Conference for something very important",
+    text: "This Wednesday we are having a nice snack & beers session after work @thelab, everybody is welcome for some beer and games, nice conversations and warm wishes about hollidays! The rest of the text is going to be nonsense because I just have to write something in order to test this layout. If you  are still reading you must be very bored. Stop procrastinating now. You have work to do. Still here? I'm gonna tell Nikos.",
+    link: "https://www.youtube.com/watch?v=JxS5E-kZc2s"
+  },
+
+];
+
