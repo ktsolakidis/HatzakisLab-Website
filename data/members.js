ab51f618300a80ad1d46ba5b8c547772449e2c21
Konstantinos Tsolakidis <ktsolakidis@Konstantinoss-MacBook-Pro.local>
2024-01-19 12:01:08 +0100

    First10

--- data/members.js
+++ data/members.js
@@ -29,7 +29,7 @@ export const members = [
       name: "Tania Darphorn",
       img: "/tania.jpg",
       email: "tsd@chem.ku.dk",
-      desc: "Project Coordinator",
+      desc: "Center Coordinator",
       blank: true
   },
   {
